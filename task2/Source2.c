fb0bc455a77ad324bc412d09a9b779692d0b1790
Nikolai0902 <kbus94@yandex.ru>
2022-09-24 17:28:55 +0300

    Задача 3

---
+++ task2/Source2.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+
+/*
+Написать программу, которая:
+по двум данным массивам определяет те элементы первого массива, которых нет во втором.».
+*/
+int main(void) {
+	int arr1[10];
+	int arr2[10];
+
+	return 0;
+}
