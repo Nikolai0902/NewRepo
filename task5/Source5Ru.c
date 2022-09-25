46e5bea6d040b9c1c5558a91a37c70851596022c
Nikolai0902 <kbus94@yandex.ru>
2022-09-25 15:36:37 +0300

    Задача 6

---
+++ task5/Source5Ru.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+
+/*
+Написать программу, которая:
+выводит строку, изображающую десятичную запись числа, 
+данного в двоичной системе счисления.
+*/
+int main(void) {
+
+	setlocale(LC_ALL, "Rus");
+ 
+    char q;
+    int res = 0;
+    printf("Введите число в двоичной системе счисления: \n");
+
+    while (scanf("%c", &q) == 1) // цикл while перебирает введенное с консоли значение.
+    {
+        if (q != '0' && q != '1')
+        {
+                break;    
+        }
+        res = res * 2 + (q - '0'); 
+    }
+    printf("Результат: %i\n", res);
+
+    return 0;
+}
